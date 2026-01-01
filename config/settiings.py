2c31b8d3c02fbf2a3eece4e9ed1be40de538e71b
JuraevSherzod <juraevsherzod9943@gmail.com>
2026-01-02 00:35:09 +0500

    Setting class wrote for .env file

---
+++ config/settiings.py
@@ -0,0 +1,16 @@
+from pydantic_settings import BaseSettings
+
+
+class Setting(BaseSettings):
+
+    database_url: str
+    access_token_minutes: int
+    refresh_token_days: int
+    secret_key: str
+
+    class Config:
+        env_file = '.env'
+        env_file_encoding = 'utf-8'
+
+
+settings = Setting()
