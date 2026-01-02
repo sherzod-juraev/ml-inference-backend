8fedd1a02f6c553ae4202a018706f6f18c23f7dc
JuraevSherzod <juraevsherzod9943@gmail.com>
2026-01-02 22:47:13 +0500

    changed connection.py file in api/core/redis

--- api/core/redis/connection.py
+++ api/core/redis/connection.py
@@ -1,8 +1,8 @@
-import aioredis
+from redis.asyncio import Redis
 from config import setting
 
 
-redis = aioredis.from_url(
+redis = Redis.from_url(
     url=setting.aioredis_url,
     encoding='utf-8',
     decode_responses=True
