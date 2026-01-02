baae80a348b150384e27d9991a0dfed91517117b
JuraevSherzod <juraevsherzod9943@gmail.com>
2026-01-02 12:43:06 +0500

    engine and session wrote in api/database/connection.py

---
+++ api/database/connection.py
@@ -0,0 +1,20 @@
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from sqlalchemy.orm import declarative_base
+from config import setting
+
+
+async_engine = create_async_engine(
+    url=setting.database_url,
+    pool_size=50,
+    max_overflow=50,
+    pool_recycle=1800,
+    pool_timeout=3,
+    pool_pre_ping=True
+)
+
+
+async_session = async_sessionmaker(
+    async_engine,
+    expire_on_commit=False
+)
+Base = declarative_base()
